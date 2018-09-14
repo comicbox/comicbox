4e842fb8c43ba789408beed55cb5918333ab252c
Adam Bibby <adam@localhost.localdomain>
2018-09-14 10:55:29 -0400

    book mutations are working

--- comicboxd/app/controller/bookColtroller.go
+++ comicboxd/app/controller/bookColtroller.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"bitbucket.org/zwzn/comicbox/comicboxd/app/database"
 	"bitbucket.org/zwzn/comicbox/comicboxd/app/gql"
@@ -40,7 +41,7 @@ var BookType = graphql.NewObject(graphql.ObjectConfig{
 		},
 		"summary": &graphql.Field{
 			Type:    graphql.String,
-			Resolve: gql.ResolveVal("Series"),
+			Resolve: gql.ResolveVal("Summary"),
 		},
 		"story_arc": &graphql.Field{
 			Type:    graphql.String,
@@ -343,9 +344,6 @@ var BookInput = graphql.NewInputObject(graphql.InputObjectConfig{
 		"rating": &graphql.InputObjectFieldConfig{
 			Type: graphql.Float,
 		},
-		"read": &graphql.InputObjectFieldConfig{
-			Type: graphql.Boolean,
-		},
 		"pages": &graphql.InputObjectFieldConfig{
 			Type: graphql.NewList(PageInput),
 		},
@@ -358,8 +356,11 @@ var PageInput = graphql.NewInputObject(graphql.InputObjectConfig{
 		"url": &graphql.InputObjectFieldConfig{
 			Type: graphql.String,
 		},
-		"number": &graphql.InputObjectFieldConfig{
+		"file_number": &graphql.InputObjectFieldConfig{
 			Type: graphql.Int,
+		},
+		"type": &graphql.InputObjectFieldConfig{
+			Type: PageTypeEnum,
 		},
 	},
 })
@@ -377,24 +378,109 @@ var BookMutations = graphql.Fields{
 		},
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 			var err error
+			var query string
 			c := gql.Ctx(p)
+
+			if (c.User.ID == uuid.UUID{}) {
+				return nil, fmt.Errorf("you must be logged in to mutate books")
+			}
 			book := &model.BookUserBook{}
-			fmt.Printf("%#v\n", p.Args["book"])
+
 			id, old := p.Args["id"]
-			if old {
-				// run an sql update
-				return nil, fmt.Errorf("make the update adam")
-			} else {
-				book.ID, err = uuid.NewRandom()
+
+			if !old {
+				id, err = uuid.NewRandom()
 				if err != nil {
 					return nil, err
 				}
-				id = book.ID
-				//run sql insert with new id
-				return nil, fmt.Errorf("make the insert adam")
 			}
 
-			err = database.DB.Get(book, `SELECT * FROM "book_user_book" where  user_id=? and id=? limit 1;`, c.User.ID, id)
+			bookCols := model.GetTags(model.Book{}, "db")
+			userBookCols := model.GetTags(model.UserBook{}, "db")
+			for table, modelCols := range map[string][]string{"book": bookCols, "user_book": userBookCols} {
+
+				cols := []string{}
+				values := []interface{}{}
+				bookMap := p.Args["book"].(map[string]interface{})
+
+				for _, col := range modelCols {
+					value, ok := bookMap[col]
+					if ok {
+						cols = append(cols, col)
+
+						switch value.(type) {
+						case int, float64, string:
+							values = append(values, value)
+						default:
+							jsonVal, err := json.Marshal(value)
+							if err != nil {
+								return nil, err
+							}
+							values = append(values, string(jsonVal))
+
+							if col == "pages" {
+								cols = append(cols, "page_count")
+								values = append(values, len(value.([]interface{})))
+
+							}
+						}
+					}
+				}
+				if len(cols) == 0 {
+					continue
+				}
+				numRows := 1
+				if table == "user_book" {
+					err = database.DB.Get(&numRows, `select count(*) from "user_book" where user_id=? and book_id=?;`, c.User.ID, id)
+					if err != nil {
+						return nil, err
+					}
+				}
+
+				if old && numRows > 0 {
+					wheres := []string{}
+					for _, col := range cols {
+						wheres = append(wheres, fmt.Sprintf("%s=?", col))
+					}
+					where := ""
+					if table == "book" {
+						where = "id=?"
+						values = append(values, id)
+					} else {
+						where = "book_id=? and user_id=?"
+						values = append(values, id)
+						values = append(values, c.User.ID)
+					}
+					query = fmt.Sprintf(`update "%s" set %s where %s;`, table, strings.Join(wheres, ", "), where)
+				} else {
+					questions := []string{}
+					for range values {
+						questions = append(questions, "?")
+					}
+					if table == "book" {
+						cols = append(cols, "id")
+						questions = append(questions, "?")
+						values = append(values, id)
+					} else {
+						cols = append(cols, "book_id")
+						questions = append(questions, "?")
+						values = append(values, id)
+
+						cols = append(cols, "user_id")
+						questions = append(questions, "?")
+						values = append(values, c.User.ID)
+					}
+
+					query = fmt.Sprintf(`insert into "%s" (%s) values (%s);`, table, strings.Join(cols, ", "), strings.Join(questions, ", "))
+
+				}
+				_, err = c.DB.Exec(query, values...)
+				if err != nil {
+					return nil, err
+				}
+			}
+
+			err = database.DB.Get(book, `select * from "book_user_book" where  user_id=? and id=? limit 1;`, c.User.ID, id)
 			if err == sql.ErrNoRows {
 				return nil, nil
 			} else if err != nil {
