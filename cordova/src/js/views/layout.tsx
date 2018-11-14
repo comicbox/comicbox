c6d2a918fdbde5310495be32335abadb38dee67d
Adam Bibby <adam_bibby@hotmail.com>
2018-11-14 10:50:28 -0500

    removed drawr

--- cordova/src/js/views/layout.tsx
+++ cordova/src/js/views/layout.tsx
@@ -2,12 +2,7 @@ import autobind from 'autobind-decorator'
 import * as s from 'css/layout.scss'
 import { historyPop, historyPrevious } from 'js/history'
 import { Component, h } from 'preact'
-import 'preact-material-components/Button/style.css'
-import Drawer from 'preact-material-components/Drawer'
-import 'preact-material-components/Drawer/style.css'
 import Icon from 'preact-material-components/Icon'
-import List from 'preact-material-components/List'
-import 'preact-material-components/List/style.css'
 import TopAppBar from 'preact-material-components/TopAppBar'
 import 'preact-material-components/TopAppBar/style.css'
 import { Link, route } from 'preact-router'
@@ -91,56 +86,24 @@ export default class Layout extends Component<Props, State> {
                 </TopAppBar.Row>
             </TopAppBar>
 
-            {/* <Drawer modal={true} open={this.state.drawerOpened} onClose={this.drawerClosed}>
-                <Drawer.DrawerHeader className='mdc-theme--primary-bg'>
-                    Drawer Header
-                </Drawer.DrawerHeader>
-                <Drawer.DrawerContent>
-                    <List>
-                        {this.menu.map((item, i) =>
-                            <Link key={i} href={item.href} onClick={this.toggleDrawer}>
-                                <List.LinkItem
-                                    className={location.pathname === item.href ? 'mdc-list-item--activated' : ''}
-                                >
-                                    <List.ItemGraphic>{item.icon}</List.ItemGraphic>
-                                    {item.name}
-                                </List.LinkItem>
-                            </Link>,
-                        )}
-                    </List>
-                </Drawer.DrawerContent>
-            </Drawer> */}
-
             <main class={s.main}>
                 {this.props.children}
             </main>
 
             <div class={s.bottomBar}>
-                {this.menu.map((item, i) => <Link
-                    key={i}
-                    href={item.href}
-                    class={s.link + ' ' + (location.hash === '#' + item.href ? s.active : '')}
-                >
-                    <Icon class={s.icon}>{item.icon}</Icon>
-                    <div class={s.title}>{item.name}</div>
-                </Link>)}
+                {this.menu.map((item, i) => (
+                    <Link
+                        key={i}
+                        href={item.href}
+                        class={s.link + ' ' + (location.hash === '#' + item.href ? s.active : '')}
+                    >
+                        <Icon class={s.icon}>{item.icon}</Icon>
+                        <div class={s.title}>{item.name}</div>
+                    </Link>
+                ))}
             </div>
         </div >
     }
-
-    // @autobind
-    // private toggleDrawer() {
-    //     this.setState({
-    //         drawerOpened: !this.state.drawerOpened,
-    //     })
-    // }
-
-    // @autobind
-    // private drawerClosed() {
-    //     this.setState({
-    //         drawerOpened: false,
-    //     })
-    // }
 
     @autobind
     private btnBack(e: Event) {
