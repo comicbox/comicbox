b4f669dd8da73b64ddf82b6a84079a9b4ca391dd
Adam Bibby <adam_bibby@hotmail.com>
2018-11-17 11:35:46 -0500

    dont change the url on web

--- cordova/src/js/url.ts
+++ cordova/src/js/url.ts
@@ -1,7 +1,7 @@
 import Options from 'js/options'
 
 export default async function url(path: string): Promise<string> {
-    if (location.protocol === 'file:' || true) {
+    if (location.protocol === 'file:') {
         let origin = await Options.getOrigin()
         if (!origin) {
             origin = prompt('what origin are you useing? e.g. "http://192.168.1.10:8080"')
