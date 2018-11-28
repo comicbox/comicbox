716f3be8b4e91f2502865bb1e4de8fb085ec9971
Adam Bibby <adam_bibby@hotmail.com>
2018-11-28 09:46:20 -0500

    added basic desktop layout

--- cordova/src/js/components/top-bar.tsx
+++ cordova/src/js/components/top-bar.tsx
@@ -59,11 +59,9 @@ export default class TopBar extends Component<Props & JSX.HTMLAttributes> {
             this.header.style.top = this.offset + 'px'
 
             if (scrollTop + this.offset === 0 && this.props.clear) {
-                this.header.style.backgroundColor = 'rgba(0,0,0,0)'
-                this.header.style.backgroundImage = 'linear-gradient(black, rgba(0,0,0,0))'
+                this.header.classList.add(s.hidden)
             } else {
-                this.header.style.backgroundColor = null
-                this.header.style.backgroundImage = null
+                this.header.classList.remove(s.hidden)
             }
 
             this.lastScrollTop = scrollTop
