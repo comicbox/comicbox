7fba2568246747a8d90958cb545115980242a529
Adam Bibby <Adam_Bibby@hotmail.com>
2019-02-09 07:58:07 -0500

    fixed max page

--- comicboxd/migrations/1549682743_fix_user_book_triggers.up.sql
+++ comicboxd/migrations/1549682743_fix_user_book_triggers.up.sql
@@ -25,8 +25,8 @@ begin
     update 
         user_book
     set 
-        current_page = min(current_page, (select page_count from book where id = user_book.book_id)),
-        last_page_read = min(max(last_page_read, current_page), (select page_count from book where id = user_book.book_id))
+        current_page = min(current_page, (select page_count from book where id = user_book.book_id) - 1),
+        last_page_read = min(max(last_page_read, current_page), (select page_count from book where id = user_book.book_id) - 1)
     where  
         user_book.book_id = new.book_id and
         user_book.user_id = new.user_id;
