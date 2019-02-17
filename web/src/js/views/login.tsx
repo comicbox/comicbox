e9bab9e1e6f4fe584951221eeb3fed93647ec3b3
Adam Bibby <Adam_Bibby@hotmail.com>
2019-02-17 09:46:20 -0500

    show navigation options on login if guest mode is enabled

--- web/src/js/views/login.tsx
+++ web/src/js/views/login.tsx
@@ -2,15 +2,34 @@ import autobind from 'autobind-decorator'
 import * as s from 'css/login.scss'
 import { login } from 'js/auth'
 import Form from 'js/components/form'
+import Book from 'js/model/book'
 import { Component, h } from 'preact'
 import Btn from 'preact-material-components/Button'
 import TextField from 'preact-material-components/TextField'
 import { route } from 'preact-router'
+import Layout from './layout'
 
-export default class Login extends Component {
+interface State {
+    guestMode: boolean
+}
+
+export default class Login extends Component<{}, State> {
+
+    constructor(props: {}) {
+        super(props)
+
+        this.state = {
+            guestMode: false,
+        }
+
+        Book.take(0).get()
+            .then(() => this.setState({ guestMode: true }))
+            .catch(() => this.setState({ guestMode: false }))
+
+    }
 
     public render() {
-        return <div class={s.form}>
+        const content = <div class={s.form + ' ' + (this.state.guestMode ? s.guestMode : '')}>
             <h1>Login</h1>
             <Form submit={this.login}>
 
@@ -28,6 +47,14 @@ export default class Login extends Component {
                 <Btn raised type='submit' class={s.login}>Login</Btn>
             </Form>
         </div >
+
+        if (this.state.guestMode) {
+            return <Layout backLink='/' breadcrumbs={[]}>
+                {content}
+            </Layout>
+        }
+        return content
+
     }
 
     @autobind
