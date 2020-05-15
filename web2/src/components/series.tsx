9e59b7d92c1f1b54da3c2e72bd6948d5a6e16a73
Adam Bibby <adam_bibby@hotmail.com>
2020-05-15 16:27:32 -0400

    add some basic style

--- web2/src/components/series.tsx
+++ web2/src/components/series.tsx
@@ -2,9 +2,10 @@ import { FunctionalComponent, h } from "preact";
 import { useAsync } from '../async-hook';
 import { db, Book } from "../db";
 import Dexie from "dexie";
+import styles from "./series.module.scss";
 
 async function getSeries(): Promise<string[]> {
-    return Array.from(await db.books.orderBy('series').uniqueKeys()).map(String)
+    return Array.from(await db.books.orderBy('series').uniqueKeys()).map(String).slice(0, 20)
 }
 
 async function getFirstBook(series: string): Promise<Book | undefined> {
@@ -32,8 +33,8 @@ export const Series: FunctionalComponent<{ series: string }> = ({ series }) => {
             Loading...
         </div>
     }
-    return <div>
-        <img src={`/api/v1/book/${book.id}/page/0.jpg?height=200`} alt="" loading="lazy" />
+    return <div className={styles.series}>
+        <img src={`/api/v1/book/${book.id}/page/0.jpg?height=200`} alt="" />
         {book.series} - #{book.chapter} V{book.volume}
     </div>
 }
